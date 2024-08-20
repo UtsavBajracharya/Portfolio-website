1171ad39d70f0583f07e53c5968a883eaa22c5ab
Utsav <utsavbajra619@gmail.com>
2024-08-20 14:26:27 -0400

    added and imported js scripts

---
+++ script.js
@@ -0,0 +1,31 @@
+// Script for header scroll
+document.addEventListener('scroll', () => {
+    const nav = document.querySelector('nav')
+
+    if (window.scrollY > 0) {
+        nav.classList.add('scrolled');
+    } else {
+        nav.classList.remove('scrolled');
+    }
+});
+
+
+// Script for mobile nav menu
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll("li");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
