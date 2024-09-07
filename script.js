97f62cf18a4a6b2e71104fda28d1d2dfd9502ae0
Utsav <utsavbajra619@gmail.com>
2024-09-07 18:26:54 -0400

    added image gallery with filter with js functions

--- script.js
+++ script.js
@@ -117,6 +117,38 @@ for (const target of targets) {
   observer.observe(target);
 }
 
+// Script for project filter
+let sortBtn = document.querySelector('.filter-menu').children;
+let sortItem = document.querySelector('.filter-item').children;
+
+for(let i = 0; i < sortBtn.length; i++){
+    sortBtn[i].addEventListener('click', function(){
+        for(let j = 0; j< sortBtn.length; j++){
+            sortBtn[j].classList.remove('current');
+        }
+
+        this.classList.add('current');
+        
+
+        let dataFilter = this.getAttribute('data-filter');
+
+        for(let k = 0; k < sortItem.length; k++){
+            sortItem[k].classList.remove('active');
+            sortItem[k].classList.add('hide');
+
+            if(sortItem[k].getAttribute('data-item') == dataFilter || dataFilter == "all"){
+                sortItem[k].classList.remove('hide');
+                sortItem[k].classList.add('active');
+            }
+        }
+    });
+}
+
+
+
+
+
+
 
 
 
